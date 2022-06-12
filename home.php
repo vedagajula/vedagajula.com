4a458a1d4b4db569cad595ace43b9ca3c20b6b75
hellorishav <79098022+hellorishav@users.noreply.github.com>
2022-06-11 22:53:05 -0500

    Dumping all files to initial website

---
+++ home.php
@@ -0,0 +1,42 @@
+<HTML>
+    <HEAD>
+        <TITLE>
+            Veda's Portfolio
+        </TITLE>
+        <link rel="stylesheet" href="https://rishavkumar.io/styles.css">
+    </HEAD>
+    <section class="section" style="background-color: #FFB6C1">
+
+    <body>
+
+    <div class="columns">
+    <div class="column">
+    
+    <h2 class="title">Hey, I'm Veda. 👋</h2>
+
+    <p class="mt-5"></p>
+
+    <p>I'm a Biomedical Engineering Undergraduate Student at <b>Arizona State University, Tempe</b>.
+    Currently, I'm working at the AzCATI (Arizona Center of Algae Technology and Innovation) of Arizona State University as a Lab Assistant. 
+    </p>
+
+    <p class="mt-5"></p>
+
+    <p>You can reach me at <b><a href="mailto:hi@vedagajula.com">hi@vedagajula.com</a></b>.</p>
+
+    <p class="mt-5"></p>
+
+     <button onclick="document.location.href='https://rishavkumar.io/veda/resume.pdf'" class="button is-link">Résumé</button>
+
+    </div>
+    <div class="column" align="right">
+    
+    <img src="https://rishavkumar.io/assets/faculty_headshot.jpg" style="width: 300px; border-radius: 14px !important;">
+
+    </div>
+    </div>
+
+    </body>
+
+    </section>
+</HTML>
---
+++ project3.php
@@ -0,0 +1,79 @@
+<html>
+    <head>
+        <title>Addiction to Short Video Content Format Applications & their effect on today's youth</title>
+        <link rel="stylesheet" href="https://rishavkumar.io/styles.css">
+    </head>
+    <body>
+        <section class="section" style="max-width: 550px;">
+
+        <button onclick="popup()" class="button is-link" style="position: fixed; bottom: 3px;"><i class="fa-solid fa-pen"></i>Sign the Pledge to reach more people</button>
+
+        <h2 class="title">Addiction to Short Video Content Format Applications & their effect on today's youth</h2>
+        <div align="center"><img src='https://rishavkumar.io/veda/assets/tiktok1.jpeg'></div><br><br>
+        <p>
+            Short video format applications are essentially applications that live on someone's mobile phone that gives access to millions of videos that are short in length, usually 60 seconds. These live on your mobile phones, that makes you prone to launch these apps whenever you feel bored.
+            Many people have raised awareness about this issue of <a href="https://fixingport.com/why-is-tiktok-so-addictive/">being addicted to TikTok and similar short video format applications</a> that are creating a huge distraction.
+        </p>
+        <br>
+        <p>
+            These applications have become the reason for today's youth to become addicted to these types of content which is making them more and more addicted and making their attention span shorter. This has been tested and 
+            has been proven that these types of applications where people are able to consume contents that consists of jump cuts and rapid motion within a short span releases dopamine
+            in their brains and causes a lot of stress release that usually is caused by smoking. Experts recommend a process called "Dopamine Detox" to get rid of these problems. These kinds of 
+            content have been proven to decrease the attention span of many people thus hindering their studies to a great extent.
+        </p>
+        <br>
+        <h2 class="title">Problem and Addiction</h2>
+        <div align="center"><img src='https://rishavkumar.io/veda/assets/tiktok4.jpeg'></div><br><br>
+        <p>
+            Short video format applications like TikTok are present on your phone, especially designed such that you launch these apps whenever you feel bored and immediately distracts you from your work and makes you lose concentration and progress at your current work you're assigned to.
+            These applications have been having a negative effect on today's youth, and people have been raising awareness about how <a href="https://www.rubiconline.com/tiktok-is-corrupting-the-minds-of-the-youth/"> short video format applications are corrupting today's youth.</a>
+        </p>
+        <br>
+        <h2 class="title">Affected population and the significance of the matter</h2>
+        <div align="center"><img src='https://rishavkumar.io/veda/assets/tiktok2.jpeg'></div><br><br>
+        <p>
+            Applications like TikTok, Instagram Reels, etc. are mainly affecting teenagers of today. They are being addicted to these kind of applciations and spend hours of their time in watching videos that are of this format.
+            This reduces their attention span, making them lose focus on studies and as a result, their grades are going down as well.
+        </p>
+        <br>
+        <h2 class="title">Science behind the addiction to short content</h2>
+        <div align="center"><img src='https://rishavkumar.io/veda/assets/tiktok3.webp'></div><br><br>
+        <p>
+            Addiction to these kind of short-video format applications are <a href="https://sitn.hms.harvard.edu/flash/2018/dopamine-smartphones-battle-time/"> due to the dopamine release</a> that happens in our brain once we scroll through these videos. It has been found that, addiction to this kind of social media releases 
+            the same amount of dopamine that would have been released if a person was smoking. This is something serious to consider and improve our lives from. It essentially means that it is no less an addiction than being addicted to smoking and that is something to be worried about and bring 
+            about a change.
+        </p>
+        <br>
+        <h2 class="title">TikTok's Algorithm & Wall Street Journal's Bot Experiment</h2>
+        <div align="center"><iframe width="560" height="315" src="https://www.youtube.com/embed/nfczi2cI6Cs" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div><br><br>
+        <p>
+            I'd like to present to you one of the most important videos that helped me drive this article for everyone. This is a YouTube video posted by Wall Street Journal, one of the top media agencies in the world. 
+            Here, they show an experiment that is being performed on TikTok to know more about their algorithm which is heavily guarded by it's parent company, ByteDance. Although the company mentions that they do not collect 
+            much data about all of the users, Wall Street Journal's programmed bots find something else which is really disturbing. <br><br>
+            Looking into such content and research the provides insight into how the "rabbit-hole" of TikTok works is one of the biggest eye-openers that I wanted to include for today's youth to understand how it is ruining their life. 
+            For such applications, the algorithms are the main part that influence the entirety of their revenue and user retention. Making these algorithms devise such evil capabilities to drag people into depression (as mentioned in Wall Street Journal's research video) 
+            is one of the things I strongly stand against for.
+        </p>
+        <br>        
+        <h2 class="title">Citations</h2>
+        <p>
+            1) Meral, Kevser Zeynep. "Social Media Short Video-Sharing TikTok Application and Ethics: Data Privacy and Addiction Issues." Multidisciplinary Approaches to Ethics in the Digital Era, edited by Meliha Nurdan Taskiran and Fatih Pinarbaşi, IGI Global, 2021, pp. 147-165. https://doi.org/10.4018/978-1-7998-4117-3.ch010<br><br>
+            2) Zhou, Yi-Mou, and Sang-Ho Lee. “A Study on the Influencing Factors on Flow & Addiction of Tiktok Service Users.” Journal of the Korea Convergence Society, vol. 12, no. 3, 한국융합학회, Mar. 2021, pp. 125–132, doi:10.15207/JKCS.2021.12.3.125.<br><br>
+            3) Conghui Su, Hui Zhou, Liangyu Gong, Binyu Teng, Fengji Geng, Yuzheng Hu, Viewing personalized video clips recommended by TikTok activates default mode network and ventral tegmental area, NeuroImage, Volume 237, 2021, 118136, ISSN 1053-8119, https://doi.org/10.1016/j.neuroimage.2021.118136. (https://www.sciencedirect.com/science/article/pii/S1053811921004134)<br><br>
+            4) Understanding Young Adults Tiktok Usage - communication. (n.d.). Retrieved April 26, 2022, from https://communication.ucsd.edu/_files/undergrad/yang-yuxin-understanding-young-adults-tiktok-usage.pdf <br><br>
+            5) Kumar V D, Prabha M S. Getting glued to TikTok® – Undermining the psychology behind widespread inclination toward dub-mashed videos. Arch Ment Health [serial online] 2019 [cited 2022 Apr 26];20:76-7. Available from: https://www.amhonline.org/text.asp?2019/20/2/76/275210<br><br>
+        </p>
+        <br>
+        
+        </section>
+    </body>
+    <script>
+
+        // Display message for Pledge Button
+        
+        function popup() {
+            alert("Thank you for signing this pledge. This will help the article and the message reach more and more people around the world.");
+        }
+
+    </script>
+</html>
